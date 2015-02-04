8c6cc81f8362dae54ac199ff8c877bfa4c4f0fb3
sam <sam@silver42.karoo.co.uk>
2015-02-04 17:53:35 +0000

    Gotta refresh ad on ui thread

--- dependencies/samcodes-admob/src/com/samcodes/admob/AdMobExtension.java
+++ dependencies/samcodes-admob/src/com/samcodes/admob/AdMobExtension.java
@@ -101,28 +101,32 @@ public class AdMobExtension extends Extension
 		final AdView view = getBannerViewForUnitId(id);
 		
 		if(view != null) {
-			AdRequest request = null;
-			
-			if(testDeviceId != "null") {
-				request = new AdRequest.Builder()
-				.addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-				.addTestDevice(testDeviceId)
-				.build();
-			} else {
-				request = new AdRequest.Builder()
-				.addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-				.build();
+			mainActivity.runOnUiThread(new Runnable() {
+				public void run() {
+					AdRequest request = null;
+					
+					if(testDeviceId != "null") {
+						request = new AdRequest.Builder()
+						.addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+						.addTestDevice(testDeviceId)
+						.build();
+					} else {
+						request = new AdRequest.Builder()
+						.addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+						.build();
+					}
+					
+					if(view.getAdListener() == null) {
+						view.setAdListener(new BannerListener(id));
+					} else if(view.getAdListener().getClass().equals(BannerListener.class) == false) {
+						view.setAdListener(new BannerListener(id));
+					}
+					
+					Log.d(TAG, "Preparing to show banner with id " + id);
+					
+					view.loadAd(request);
+				});
 			}
-			
-			if(view.getAdListener() == null) {
-				view.setAdListener(new BannerListener(id));
-			} else if(view.getAdListener().getClass().equals(BannerListener.class) == false) {
-				view.setAdListener(new BannerListener(id));
-			}
-			
-			Log.d(TAG, "Preparing to show banner with id " + id);
-			
-			view.loadAd(request);
 		}
 	}
 	
