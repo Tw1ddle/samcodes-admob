88533e7f26e99e810da88356798e7fbcf0a7f866
sam <sam@silver42.karoo.co.uk>
2015-02-04 00:31:07 +0000

    Minor formatting

--- project/include/SamcodesAdMob.h
+++ project/include/SamcodesAdMob.h
@@ -1,12 +1,12 @@
-#ifndef CHARTBOOSTEXT_H
-#define CHARTBOOSTEXT_H
+#ifndef SAMCODESADMOB_H
+#define SAMCODESADMOB_H
 
 namespace samcodesadmob
 {
-    void initAdMob(const char* testDeviceHash);
+	void initAdMob(const char* testDeviceHash);
 	void showInterstitial(const char* location);
-    void cacheInterstitial(const char* location);
-    bool hasCachedInterstitial(const char* location);
+	void cacheInterstitial(const char* location);
+	bool hasCachedInterstitial(const char* location);
 	void showBanner(const char* location);
 	void hideBanner(const char* location);
 }
