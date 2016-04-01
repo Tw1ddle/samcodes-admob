8bcbaf76cda3f7c1aacf9d4252ce08773a17a43e
sam <sam@silver42.karoo.co.uk>
2016-04-01 21:54:17 +0100

    + ndll build script

---
+++ project/rebuild_ndlls.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+haxelib run hxcpp Build.xml clean
+haxelib run hxcpp Build.xml -Diphoneos
+haxelib run hxcpp Build.xml -Diphoneos -DHXCPP_ARMV7
+haxelib run hxcpp Build.xml -Diphoneos -DHXCPP_ARM64
+haxelib run hxcpp Build.xml -Diphonesim
+haxelib run hxcpp Build.xml -Diphonesim -DHXCPP_M64
